c4d2c7693886a04f8ec055d4cf0e13312e5c53fd
Mostafa <mostafa.e.mansour@gmail.com>
2017-01-31 16:45:32 +0300

    Landmarks

--- test.m
+++ test.m
@@ -8,35 +8,38 @@ samples=30000;
 a=0.25;
 width_x=3;
 length_y=3;
-x_real=repmat(a,1,realizations)+repmat((width_x-2*a),1,realizations).*rand(1,realizations);
-y_real=repmat(a,1,realizations)+repmat((length_y-2*a),1,realizations).*rand(1,realizations);
+position_real=repmat([a;a],1,realizations)+repmat([(width_x-2*a);(length_y-2*a)],1,realizations).*rand(2,realizations);
+% location_real=repmat(a,1,realizations)+repmat((width_x-2*a),1,realizations).*rand(1,realizations);
+% y_real=repmat(a,1,realizations)+repmat((length_y-2*a),1,realizations).*rand(1,realizations);
 sigma2=zeros(2,m,realizations);
 parfor itr=1:realizations
     disp(['realization number ' num2str(itr) ]);
-    [X(:,:,itr),P(:,:,:,itr)]=getBootStrapIndoorKnownCourse(x_real(itr),y_real(itr),samples,width_x,length_y);
-    sigma2(:,:,itr)=(X(:,:,itr)-repmat([x_real(itr);y_real(itr)],1,m));
+    [X(:,:,itr),P(:,:,:,itr)]=perceivingLandmarks(position_real(1,itr),position_real(2,itr),samples,width_x,length_y);
+    sigma2(:,:,itr)=(X(:,:,itr)-repmat([position_real(1,itr);position_real(2,itr)],1,m));
     close all;
 end
 
-sigma=sqrt(sum(sigma2.^2,3)./(realizations-1));
+sigma=sqrt(sum(sigma2.^2,3)./(realizations));
 P__=sum(P,4)./realizations;
 figure (7)
-subplot(233)
+subplot(231)
 hold on
-plot(linspace(1,m,m),sigma(1,:),'r',linspace(1,m,m),sqrt(squeeze(P__(1,1,:))),'b');
-subplot(236)
+plot(linspace(1,m,m),sigma(1,:),'.-r',linspace(1,m,m),sqrt(squeeze(P__(1,1,:))),'.-b');
+subplot(234)
 hold on
-plot(linspace(1,m,m),sigma(2,:),'r',linspace(1,m,m),sqrt(squeeze(P__(2,2,:))),'b');
+plot(linspace(1,m,m),sigma(2,:),'.-r',linspace(1,m,m),sqrt(squeeze(P__(2,2,:))),'.-b');
 
-figure(4) 
+figure (8)
 hold on
-subplot(232)
-plot(linspace(1,m,m),abs(sigma(1,:)'-sqrt(squeeze(P__(1,1,:)))),'b');%,'r',linspace(1,m,m),sqrt(squeeze(P__(1,1,:))),'b');
+subplot(231)
+hold on
+plot(linspace(1,m,m),(sigma(1,:)'-sqrt(squeeze(P__(1,1,:)))),'r');%,'r',linspace(1,m,m),sqrt(squeeze(P__(1,1,:))),'b');
 grid on,
 xlabel('Number of Meaurements')
 ylabel('Difference')
-subplot(235)
-plot(linspace(1,m,m),abs(sigma(2,:)'-sqrt(squeeze(P__(2,2,:)))),'b');
+subplot(234)
+hold on
+plot(linspace(1,m,m),(sigma(2,:)'-sqrt(squeeze(P__(2,2,:)))),'r');
 grid on,
 xlabel('Number of Meaurements')
 ylabel('Difference')
